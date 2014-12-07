60ddf87d6d0c3a795293f167964d528c65f0d838
chrishenyard <chenyard@gmail.com>
2014-12-07 10:00:47 -0700

    Initial commit of exe

---
+++ ExeLifetime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using log4net;
+
+namespace ExeLifetime {
+	class Program {
+		private static ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+		static void Main(string[] args) {
+			log4net.Config.XmlConfigurator.Configure();
+			_log.Info("Exe running in main");
+			_log.Info("Press any key to exit");
+			Console.ReadKey();
+		}
+	}
+}
